424c47f7a81e5291fb920cf493638dddeababe83
Masum Khan <masumkhan081@gmail.com>
2024-12-14 03:29:36 +0600

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BookModule } from './book/book.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [BookModule],
+  imports: [UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/book/book.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BookController } from './book.controller';
-
-@Module({
-  controllers: [BookController]
-})
-export class BookModule {}
